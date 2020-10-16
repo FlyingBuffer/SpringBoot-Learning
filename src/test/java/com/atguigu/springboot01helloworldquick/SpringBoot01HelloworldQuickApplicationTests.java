6adcb45d264105fc07d4df3be2da0df86dfcc4c9
Jackie <50996868+JackiePeng666@users.noreply.github.com>
2020-10-16 19:35:22 +0800

    Update SpringBoot01HelloworldQuickApplicationTests.java

--- src/test/java/com/atguigu/springboot01helloworldquick/SpringBoot01HelloworldQuickApplicationTests.java
+++ src/test/java/com/atguigu/springboot01helloworldquick/SpringBoot01HelloworldQuickApplicationTests.java
@@ -1,13 +1,24 @@
-package com.atguigu.springboot01helloworldquick;
+package com.aiguigu.springboot;
 
+import com.aiguigu.springboot.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/*
+SpringBoot单元测试;
+可以在测试期间很方便的类似编码一样自动注入功能
+* */
+
 @SpringBootTest
-class SpringBoot01HelloworldQuickApplicationTests {
+class SpringBoot02ConfigApplicationTests {
+    
+    @Autowired
+    Person person;
 
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
